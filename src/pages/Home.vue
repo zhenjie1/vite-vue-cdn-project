4db43099c4804fa603e389f01dbbe65bfdba66ca
zhenjie <1304119519@qq.com>
2023-02-20 11:43:19 +0800

    add VueMacros

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,9 +1,16 @@
 <script lang="ts" setup>
+defineProps({
+  msg: {
+    type: String,
+    default: 'Vite + Vue'
+  }
+})
 </script>
 
 <template>
   <div class="containerBox">
     this is a home
+    <p>{{ msg }}</p>
   </div>
 </template>
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import Vue from '@vitejs/plugin-vue'
 import externalGlobals from 'rollup-plugin-external-globals'
+import VueMacros from 'unplugin-vue-macros/vite'
 
 const externals = {
   'vue': 'Vue',
@@ -13,7 +14,15 @@ const externals = {
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    VueMacros({
+      plugins: {
+        vue: Vue({
+          include: [/\.vue$/, /\.md$/],
+          reactivityTransform: true,
+        }),
+      },
+    }),
+
   
   ],
 
